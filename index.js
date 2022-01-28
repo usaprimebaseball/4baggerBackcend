7d87a71c4f7b3e62e0254fc31e6c9806ac268a8a
unknown <malaloosi@usaprimebaseball.com>
2022-01-27 19:05:34 -0600

    make it better

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 app.use("/user", userRouter);
 
+app.use("/", (req, res) => {
+  res.send('Hello to 4 Bagger USA');
+});
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
