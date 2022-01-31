af700264d97a0290e7640a7f27d2254652d28824
unknown <malaloosi@usaprimebaseball.com>
2022-01-31 11:12:53 -0600

    added new welcome message in server

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
+app.use("/", (req, res) => {
+  res.send("Welcome to 4Bagger Backend");
+});
 app.use("/auth", authRouter);
 app.use("/account", userRouter);
 
