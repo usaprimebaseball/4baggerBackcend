e32d8ea4547280de60a864ca91967727b8e0aad0
unknown <malaloosi@usaprimebaseball.com>
2022-01-31 11:56:19 -0600

    removed main router

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use("/auth", authRouter);
+app.use("/", authRouter);
 app.use("/account", userRouter);
 
 const PORT = process.env.PORT|| 5000;
@@ -23,4 +23,3 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
