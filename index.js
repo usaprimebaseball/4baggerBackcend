8b2cb199e92757ca2b4c9b5d0f457504fc65f0f3
unknown <malaloosi@usaprimebaseball.com>
2022-01-31 10:21:35 -0600

    modified index server

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use("/", (req, res) => {
-  res.send('Welcome!')
-});
-
-app.use("/", authRouter);
+app.use("/auth", authRouter);
 app.use("/account", userRouter);
 
 const PORT = process.env.PORT|| 5000;
