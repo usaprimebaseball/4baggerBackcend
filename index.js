88ab43bc650afbbd674d6b8e3bf183e583709405
unknown <malaloosi@usaprimebaseball.com>
2022-02-09 11:06:45 -0600

    to heruko

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import authRouter from "./routes/auth.js";
 import userRouter from "./routes/user.js";
 import eventRouter from "./routes/event.js";
 import errorRouter from "./routes/error.js";
-import successRouter from "./routes/success.js";
 
 const app = express();
 dotenv.config();
@@ -19,12 +18,12 @@ app.use(cors());
 
 app.use("/", authRouter);
 app.use("/", errorRouter);
-app.use("/", successRouter);
 app.use("/events", eventRouter);
 app.use("/account", userRouter);
 
 
 const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = "mongodb+srv://4bagger:usaprime@cluster0.usrwb.mongodb.net/users?retryWrites=true&w=majority"
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
